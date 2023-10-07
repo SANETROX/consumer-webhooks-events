831cc3899ca62d03147dce6d1d9c8be183c69b01
Vannesamonroy <vanessamonroy546@gmail.com>
2023-10-07 14:50:35 -0500

    FEAT:  add  conection mongoDb

--- consumer.py
+++ consumer.py
@@ -1,4 +1,18 @@
 import pika, json, ssl, sys
+import pymongo
+from pymongo import MongoClient
+
+try:
+    mongo_client = MongoClient('mongodb://mongo:tBhJm2OF7QtOPG5zND34@containers-us-west-119.railway.app:5610')
+    db = mongo_client['events_webhooks']
+
+    print('Conexión establecida con MongoDB')
+except ConnectionError as e:
+    # Si ocurre un error de conexión, imprime un mensaje de error y el tipo de error
+    print('Error de conexión a MongoDB:', e.args)
+except Exception as e:
+    # Captura y maneja cualquier otro error que pueda ocurrir
+    print('Error desconocido:', e.args)
 
 credentials = pika.PlainCredentials('staging', 'CAMIlodev1994')
 
@@ -13,8 +27,12 @@ parameters = pika.ConnectionParameters(host='b-1ba484aa-4d38-453d-9e5b-bcc42a51a
 def callback(ch, method, properties, body):
     print('Received in admin')
     sys.stdout.flush()
+    collection = db['events']
     data = json.loads(body)
+    data['idOrder'] = data.pop('_id')
+    collection.insert_one(data)
     print(data)
+    sys.stdout.flush()
 
 
 try: 
